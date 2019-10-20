11bf578cd39d329e009dbec755b513df1fbd9c71
Andrew Dunn <atd2005@gmail.com>
2019-10-20 02:38:07 -0400

    add auth routes

--- services/users/manage.py
+++ services/users/manage.py
@@ -38,8 +38,8 @@ def test():
 @cli.command('seed_db')
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username='michael', email="hermanmu@gmail.com", password="eightletterslong"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org", password="eightletterslong"))
     db.session.commit()
 
 @cli.command()
