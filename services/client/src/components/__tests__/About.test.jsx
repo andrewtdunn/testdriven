9b33ea8928e6b7d4fe797e763482cae8196789f9
Andrew Dunn <atd2005@gmail.com>
2019-10-20 05:10:58 -0400

    client adds routes

---
+++ services/client/src/components/__tests__/About.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import About from '../About';
+
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+test('About renders properly', () => {
+    const wrapper = shallow(<About/>);
+    const element = wrapper.find('p');
+    expect(element.length).toBe(1);
+    expect(element.text()).toBe('Add something relevant here.');
+});
+
+test('About renders a snapshot properly', () => {
+    const tree = renderer.create(<About/>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
