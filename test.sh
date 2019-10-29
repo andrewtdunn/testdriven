dc17ea2ce2839d7113aeefaffa5322d268049448
Andrew Dunn <atd2005@gmail.com>
2019-10-28 23:58:24 -0400

    configures travis for prod

--- test.sh
+++ test.sh
@@ -23,8 +23,8 @@ docker-compose down
 # run e2e tests
 docker-compose -f docker-compose-prod.yml up -d --build
 docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-./node_modules/.bin/cypress run --config baseUrl=http://localhost
-inspect $? client
+./node_modules/.bin/cypress run --config baseUrl=http://localhost NODE_OPTIONS=
+inspect $? e2e
 docker-compose -f docker-compose-prod.yml down
 
 # return proper code
