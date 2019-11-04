bcf3007d968645d58f90b206349f25aa0c6cd307
Andrew Dunn <atd2005@gmail.com>
2019-11-04 15:28:29 -0500

    fix cypress location

--- test.sh
+++ test.sh
@@ -33,7 +33,7 @@ client() {
 e2e() {
     docker-compose -f docker-compose-prod.yml up -d --build
     docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-    ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+    ./node_modules/bin/cypress run --config baseUrl=http://localhost:8084
     inspect $? e2e
     docker-compose -f docker-compose-prod.yml down
 }
