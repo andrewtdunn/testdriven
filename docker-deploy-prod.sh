a28ac4d24402b2f7a81b1418ee2d7aa1f07b9699
Andrew Dunn <atd2005@gmail.com>
2019-11-16 18:47:39 -0500

    fix typo in prod deploy

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -26,21 +26,21 @@ then
 
         deploy_cluster() {
             # users
-            template="ecs_users_prod_taskdefinition.json
+            template="ecs_users_prod_taskdefinition.json"
             task_template=$(cat "ecs/$template")
             task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_URI $PRODUCTION_SECRET_KEY)
             echo "$task_def"
             register_definition
 
             # client
-            template="ecs_client_prod_taskdefinition.json
+            template="ecs_client_prod_taskdefinition.json"
             task_template=$(cat "ecs/$template")
             task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
             echo "$task_def"
             register_definition
 
             # users
-            template="ecs_swagger_prod_taskdefinition.json
+            template="ecs_swagger_prod_taskdefinition.json"
             task_template=$(cat "ecs/$template")
             task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
             echo "$task_def"
