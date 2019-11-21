0e6f6473d85e8f538f5e7413f8d7f5a9461c92d3
Andrew Dunn <atd2005@gmail.com>
2019-11-21 15:33:42 -0500

    add code editor

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -10,6 +10,7 @@ import Logout from "./components/Logout";
 import UserStatus from "./components/UserStatus";
 import Message from "./components/Message";
 import Footer from './components/Footer';
+import Exercises from "./components/Exercises";
 
 class App extends Component {
     constructor(){
@@ -87,7 +88,9 @@ class App extends Component {
                                     <br/>
                                     <Switch>
                                         <Route exact path='/' render={() => (
-                                            <p>Something</p>
+                                            <Exercises
+                                                isAuthenticated={this.state.isAuthenticated}
+                                            />
                                         )} />
                                         <Route exact path='/about' component={About}/>
                                         <Route exact path='/register' render={() => (
